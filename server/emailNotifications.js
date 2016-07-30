fb92d17a745ae1f725e8ba50c51c2dfc4782f264
mihyar-30614 <eng.mihyear@gmail.com>
2016-07-31 00:29:54 +0300

    changing the mailing address

--- server/emailNotifications.js
+++ server/emailNotifications.js
@@ -3,13 +3,13 @@ var smtpTransport = require('nodemailer-smtp-transport');
 var transporter = nodemailer.createTransport(smtpTransport ({
   service: "hotmail",
   auth: {
-        user: 'volunteerhub@outlook.com',
+        user: 'Khitwaorg@outlook.com',
         pass: process.env.outlookPass
   }
 }));
 
 module.exports = function(mailOptions){
-  mailOptions.from = 'VolunteerHub <volunteerhubco@gmail.com>';
+  mailOptions.from = 'Khitwa <Khitwaorg@gmail.com>';
   transporter.sendMail(mailOptions, function(error, info){
     if(error){
        console.log(error);
